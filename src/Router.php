9e80c6a9deda62c4af455beea385dba57b3cf210
Adam Averay <adam@averay.com>
2019-10-24 20:35:21 +0800

    Support setting default URL segments for generated paths

--- src/Router.php
+++ src/Router.php
@@ -5,10 +5,23 @@ namespace AdamAveray\SlimExtensions;
 
 class Router extends \Slim\Router
 {
+    private $defaultUrlSegments = [];
+
+    public function setDefaultUrlSegment(string $key, $value): void
+    {
+        $this->defaultUrlSegments[$key] = $value;
+    }
+
     public function pushGroup($pattern, $callable): RouteGroup
     {
         $group = new RouteGroup($pattern, $callable);
         $this->routeGroups[] = $group;
         return $group;
+    }
+
+    public function relativePathFor($name, array $data = [], array $queryParams = []): string
+    {
+        $data = array_merge($this->defaultUrlSegments, $data);
+        return parent::relativePathFor($name, $data, $queryParams);
     }
 }
